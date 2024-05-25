64a6b32ccf0bfebbbafb4f66f65736909ea678b3
WorapongWonglamduan <the_oo_ooo@hotmail.com>
2024-05-25 23:20:21 +0700

    update

--- src/components/layout/Navbars.js
+++ src/components/layout/Navbars.js
@@ -1,4 +1,4 @@
-// import React, { useMemo, useState } from "react";
+import React, { /* useMemo, */ useState } from "react";
 // import {
 //   LogoutOutlined,
 //   HomeOutlined,
@@ -8,7 +8,7 @@
 //   ShoppingOutlined,
 // } from "@ant-design/icons";
 import { /* Menu, */ Badge } from "antd";
-import { Link /* useNavigate */ } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 // import { useDispatch } from "react-redux";
 // import { toast } from "react-toastify";
 import { useSelector, shallowEqual } from "react-redux";
@@ -24,7 +24,7 @@ const Navbar = () => {
   );
 
   // const dispatch = useDispatch();
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
   // const [current, setCurrent] = useState("home");
 
   // const onClick = (e) => {
@@ -180,43 +180,63 @@ const Navbar = () => {
     );
   };
   const ModalSearch = () => {
+    const [text, setText] = useState("");
+
+    const handleChange = (e) => {
+      setText(e.target.value);
+    };
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      navigate("/shop?" + text, { state: { text: text } });
+      document.getElementById("closeButton").click();
+    };
+
     return (
-      <div
-        className="modal fade"
-        id="searchModal"
-        tabIndex="-1"
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-      >
-        <div className="modal-dialog modal-fullscreen">
-          <div className="modal-content rounded-0">
-            <div className="modal-header">
-              <h5 className="modal-title" id="exampleModalLabel">
-                Search by keyword
-              </h5>
-              <button
-                type="button"
-                className="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-              ></button>
-            </div>
-            <div className="modal-body d-flex align-items-center">
-              <div className="input-group w-75 mx-auto d-flex">
-                <input
-                  type="search"
-                  className="form-control p-3"
-                  placeholder="keywords"
-                  aria-describedby="search-icon-1"
-                />
-                <span id="search-icon-1" className="input-group-text p-3">
-                  <i className="fa fa-search"></i>
-                </span>
+      <form onSubmit={handleSubmit}>
+        <div
+          className="modal fade"
+          id="searchModal"
+          tabIndex="-1"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog modal-fullscreen">
+            <div className="modal-content rounded-0">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">
+                  Search by keyword
+                </h5>
+                <button
+                  type="button"
+                  id="closeButton"
+                  className="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="modal-body d-flex align-items-center">
+                <div className="input-group w-75 mx-auto d-flex">
+                  <input
+                    type="search"
+                    className="form-control p-3"
+                    placeholder="keywords"
+                    aria-describedby="search-icon-1"
+                    onChange={handleChange}
+                  />
+                  <span
+                    onClick={handleSubmit}
+                    style={{ cursor: "pointer" }}
+                    id="search-icon-1"
+                    className="input-group-text p-3"
+                  >
+                    <i className="fa fa-search"></i>
+                  </span>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </form>
     );
   };
   return (
